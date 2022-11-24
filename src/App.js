702ed6554918cba667524ca8ef0edd9965c7c03e
Nam Nguyen <ducnam1719@gmail.com>
2022-11-24 10:58:02 +0700

    install redux & setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 function App() {
-  return <div className="App">Zingmp3</div>
+  const { test, homeData } = useSelector((state) => state.app)
+
+  console.log(homeData)
+
+  return (
+    <>
+      <div className="">
+        App
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </div>
+    </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Provider } from 'react-redux'
+import reduxConfig from './redux'
+
+const store = reduxConfig()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
