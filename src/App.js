5ff8dff72e02cf7667f497dcea09176b4cb249bf
Nam Nguyen <ducnam1719@gmail.com>
2022-11-27 15:49:22 +0700

    setup router dom & nested router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { Home, Login, Public } from './containers/public/'
+import { Routes, Route } from 'react-router-dom'
+import path from './utils/path'
 
 function App() {
-  const { test, homeData } = useSelector((state) => state.app)
-
-  console.log(homeData)
-
   return (
     <>
       <div className="">
-        App
+        <Routes>
+          <Route path={path.PUBLIC} element={<Public />}>
+            <Route path={path.HOME} element={<Home />} />
+            <Route path={path.LOGIN} element={<Login />} />
+          </Route>
+        </Routes>
         <ToastContainer
           position="bottom-right"
           autoClose={5000}
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
 import reduxConfig from './redux'
+import { BrowserRouter } from 'react-router-dom'
 
 const store = reduxConfig()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 )
 
