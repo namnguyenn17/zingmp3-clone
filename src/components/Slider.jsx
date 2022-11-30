c6d01aa80dc863e6b5fd5d7cd4bab7ca0dabb006
Nam Nguyen <ducnam1719@gmail.com>
2022-11-30 12:25:47 +0700

    slick slidershow for header

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,16 +1,50 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { getArrSlider } from '../utils/helper'
 
 const Slider = () => {
   const { banner } = useSelector((state) => state.app)
+
+  useEffect(() => {
+    const sliderList = document.getElementsByClassName('slider-item')
+    let min = 0
+    let max = 2
+    const intervalId = setInterval(() => {
+      const list = getArrSlider(min, max, sliderList.length - 1)
+      for (let i = 0; i < sliderList.length; i++) {
+        if (list.some((item) => item === i)) {
+          sliderList[i].style.cssText = `display: block`
+        } else {
+          sliderList[i].style.cssText = `display: none`
+        }
+      }
+      if (min === sliderList.length - 1) {
+        min = 0
+      } else {
+        min += 1
+      }
+      if (max === sliderList.length - 1) {
+        max = 0
+      } else {
+        max += 1
+      }
+
+      console.log(list)
+    }, 1500)
+
+    return () => {
+      intervalId && clearInterval(intervalId)
+    }
+  }, [])
+
   return (
-    <div className="flex flex-col">
+    <div className="flex gap-4 w-full overflow-hidden px-[59px] pt-8 ">
       {banner?.map((item) => (
         <img
           key={item.encodeId}
           src={item.banner}
-          className="flex-1 object-contain"
+          className="slider-item flex-1 object-contain w-1/3 rounded-lg"
         />
       ))}
     </div>
--- src/utils/path.js
+++ src/utils/path.js
@@ -3,6 +3,7 @@ const path = {
   HOME: '',
   LOGIN: 'login',
   STAR: '*',
+  MY_MUSIC: 'mymusic',
 }
 
 export default path
